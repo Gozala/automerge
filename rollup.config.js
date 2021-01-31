246fa95971fd1540ddb7e0706ee20970b94c4587
Irakli Gozalishvili <contact@gozala.io>
2021-01-31 01:43:07 -0800

    generate source maps

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
   input: 'src/automerge.js',
   output: {
+    sourcemap: true,
     file: 'dist/automerge.js',
     format: 'es'
   },
